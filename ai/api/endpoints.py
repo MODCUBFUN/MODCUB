f7198c4418f6baf7008a9871ef2fb99a3ccf0c7f
SENDIX <eduardermakov769@gmail.com>
2025-11-08 02:56:01 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        FUNPAY
+import os        SENDIX
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
